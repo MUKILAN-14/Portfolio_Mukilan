218410573b12b4d0eeabc432b2f5663ffe22740d
Mukilan <mukilan671@gmail.com>
2025-06-06 22:10:03 +0530

    chngs

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,19 +1,14 @@
 export default function HomePage() {
   return (
-    <div className="h-screen bg-gradient-to-b from-[rgb(156,136,207)] to-[rgb(81,36,121)]">
+    <div className="h-full rounded-3xl bg-gradient-to-b from-[rgb(156,136,207)] to-[rgb(81,36,121)]">
       
- <div className="flex justify-between items-center px-6 py-4">
-    {/* Left */}
-    <div className="text-lg font-semibold">About</div>
-
-    {/* Center */}
-    <div className="text-lg font-semibold">Mukilan M</div>
-
-    {/* Right */}
-    <div className="text-lg font-semibold">Projects</div>
+ <div className="flex text-white font-semibold justify-between items-center px-6 py-6">
+    <a  href="#">About</a>
+    <a href="#">Conatcts</a>
+    <a href="#">Projects</a>
   </div>     
   
-       <div className="flex flex-col justify-center items-center h-full text-white text-center">
+      <div className="flex flex-col justify-center items-center h-screen text-white text-center ">
         <h1 className="text-4xl md:text-6xl font-bold drop-shadow-lg">
           Hi, I'm Mukilan M
         </h1>
